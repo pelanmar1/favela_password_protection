24344ffcbf55acf8a6b16cc7687855f48b4c94fe
Daniel Galvao Guerra <t-daguer@microsoft.com>
2017-07-24 14:25:03 -0700

    Added keylistener

---
+++ keylistener.py
@@ -0,0 +1,25 @@
+from Tkinter import *
+from datetime import datetime
+
+root = Tk()
+
+timeBetweenPresses = []
+lastTime = -1
+
+def key(event):
+    global lastTime
+    if lastTime is not -1:
+        timeBetweenPresses.append((datetime.now() - lastTime).total_seconds())
+    lastTime = datetime.now()
+    print(timeBetweenPresses)
+
+def callback(event):
+    frame.focus_set()
+    print("clicked at", event.x, event.y)
+
+frame = Frame(root, width=100, height=100)
+frame.bind("<Key>", key)
+frame.bind("<Button-1>", callback)
+frame.pack()
+
+root.mainloop()
